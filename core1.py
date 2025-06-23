ebd6759051fa5f2dfbe46a46ecb282a49843429a
ADITYA GUPTA <122161332+A-A-D-I-C-O-D-E@users.noreply.github.com>
2025-06-23 16:33:05 +0530

    v3

--- core1.py
+++ core1.py
@@ -26,7 +26,7 @@ def extract_base_domain(weburl):
         domain = '.'.join(parts[-2:])
     return domain
 
-def find_user_by_weburl(weburl, users_json='users1.json'):
+def find_user_by_weburl(weburl, users_json='users.json'):
     base = extract_base_domain(weburl)
     with open(users_json, 'r', encoding='utf-8') as file:
         users = json.load(file)
@@ -79,14 +79,14 @@ def click_login_button(driver):
 
 def get_chrome_driver():
     options = Options()
-    options.add_argument("--headless=new")  # use "new" headless mode for recent Chrome
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    
+    options.add_argument(f"--user-data-dir={tempfile.mkdtemp()}")
 
     return webdriver.Chrome(options=options)
-    
+
 def process_user_bot(client_username, weburl):
     site_data = find_user_by_weburl(weburl)
     if not site_data:
