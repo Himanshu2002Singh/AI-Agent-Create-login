bc457e99445d45fea0b7fca05e90a679313c1a0f
ADITYA GUPTA <122161332+A-A-D-I-C-O-D-E@users.noreply.github.com>
2025-06-26 15:50:08 +0530

    Update core.py

--- core.py
+++ core.py
@@ -51,6 +51,7 @@ def get_driver(headless=False):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--remote-debugging-port=9222")
+    options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-software-rasterizer")
